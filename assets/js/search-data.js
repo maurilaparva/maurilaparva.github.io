0b5087e1a85bdc5d00dfdf42f6b391871a60d2ec
maurilaparva <villa669@umn.edu>
2026-06-15 03:56:35 +0000

    Deploying to gh-pages from @ maurilaparva/maurilaparva.github.io@238616e1b284822063d69ed236eea7ae2c1cea0a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,16 +391,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-one-paper-on-human-centered-llm-uncertainty-was-accepted-by-facct-2026",
+          title: 'One paper on human-centered LLM uncertainty was accepted by FAccT 2026!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
